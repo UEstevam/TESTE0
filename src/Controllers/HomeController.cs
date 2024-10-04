c5d53f489365e2c6c4f5c8d52ffa156a4fa01b2b
Woton Sampaio <woton.sampaio@maxifrota.com.br>
2024-10-04 18:34:24 -0300

    adicionando pagina de login

---
+++ src/Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using src.Models;
+using System;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace src.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly DatabaseContext _context;
+
+        public HomeController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string email, string senha)
+        {
+            string senhaHash = GerarHashSha256(senha);
+
+            var usuario = _context.Usuarios.FirstOrDefault(u => u.Email == email && u.Senha == senhaHash);
+
+            if (usuario != null)
+            {
+                return RedirectToAction("Index");
+
+            }
+
+            ModelState.AddModelError(string.Empty, "Login inválido.");
+            return View("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Cadastro(string nome, string email, string nomeEmpresa, string senha)
+        {
+            if (_context.Usuarios.Any(u => u.Email == email))
+            {
+                // Email já registrado
+                ModelState.AddModelError(string.Empty, "O e-mail já está registrado.");
+                return View("Index");
+            }
+
+            string senhaHash = GerarHashSha256(senha);
+
+            var novoUsuario = new Usuario
+            {
+                Nome = nome,
+                Email = email,
+                Senha = senhaHash
+            };
+
+            _context.Usuarios.Add(novoUsuario);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        private string GerarHashSha256(string senha)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(senha));
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/Data/DatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using src.Models;
+
+public class DatabaseContext : DbContext
+{
+    public DatabaseContext(DbContextOptions<DatabaseContext> options): base(options)
+    {
+
+    }
+
+    public DbSet<Usuario> Usuarios { get; set; }
+}
