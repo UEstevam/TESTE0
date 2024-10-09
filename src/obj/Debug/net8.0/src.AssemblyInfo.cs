6810f59b472a30450c4853f8f72b658da4dbed07
Hugo Estevam <hugoestevam20@gmail.com>
2024-10-08 22:03:41 -0300

    Ajuste na Tela de Perfil

--- src/obj/Debug/net8.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/src.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13748170d55206902b90d18ccd7299e7d61a64b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e628fde5e3dd1c5e54bacc0864ffd778565fa13f")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
