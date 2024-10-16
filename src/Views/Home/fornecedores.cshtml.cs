ca578801b9fea46ea8203935552fee68a8e3a82e
marcellapontes1 <marcellapontesdosanjos@hotmail.com>
2024-10-15 21:01:09 -0300

    atualizando tela de fornecedore

---
+++ src/Views/Home/fornecedores.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace src.Views.Home
+{
+    public class fornecedoresModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- src/obj/Debug/net8.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/src.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e628fde5e3dd1c5e54bacc0864ffd778565fa13f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6810f59b472a30450c4853f8f72b658da4dbed07")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
