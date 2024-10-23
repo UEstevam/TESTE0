b2dd9dd8239109fc360dea84bfa88ac04316dc55
Hugo Estevam <hugoestevam20@gmail.com>
2024-10-22 21:13:11 -0300

    Atualizações

--- src/obj/Debug/net8.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/src.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+575670faf44ca2de58f0fe4c30a0eaf50d2c3d2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c80696c503742a6b666fb9f18bf4ba446501795d")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
